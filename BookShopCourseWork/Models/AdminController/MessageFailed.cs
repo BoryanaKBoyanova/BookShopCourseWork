148a3300d195834503d10c67032551893f0bc44d
Boryana Boyanova <boryana.k.boyanova@gmail.com>
2021-06-10 15:37:51 +0300

    add messages edit

--- BookShopCourseWork/Models/AdminController/MessageFailed.cs
+++ BookShopCourseWork/Models/AdminController/MessageFailed.cs
@@ -4,9 +4,9 @@ namespace BookShopCourseWork.Models.AdminController
 {
     public class MessagesFailed
     {
-        public string CreateBook => "Book has been created successfully!";
-        public string EditBook => "Book has been edited successfully!";
-        public string BookAuthorConnected => "Book has been connected to author successfully!";
-        public string BookAuthorDisconnected => "Book has been desconnected from author successfully!";
+        public string CreateBook => "Failed to create book!";
+        public string EditBook => "Failed to edit book!";
+        public string BookAuthorConnected => "Failed to connect author to book!";
+        public string BookAuthorDisconnected => "Failed to disconnect author from book!";
     }
 }
